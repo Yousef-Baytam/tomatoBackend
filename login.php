1a923aa98ec3867a1db5057e0e655b36a292e4f4
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-29 18:04:47 +0300

    Added a login api

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+include("connection.php");
+$email = $_POST["email"];
+$password = hash("sha256", $_POST["passwrod"]);
+$query = $mysqli->prepare("Select id from users where email = ? AND password = ?");
+$query->bind_param("ss", $email, $password);
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows;
+$query->bind_result($id);
+$query->fetch();
+$response = [];
+if ($num_rows == 0) {
+    $response["response"] = "User Not Found";
+} else {
+    $response["response"] = "Logged in";
+    $response["user_id"] = $id;
+}
+$json = json_encode($response);
+echo $json;
