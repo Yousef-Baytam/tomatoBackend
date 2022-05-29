98ffce2a5003fc6c43b1c4a0a6512aff3e9db889
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-29 17:44:18 +0300

    Added error handling to adding users

--- addUser.php
+++ addUser.php
@@ -12,13 +12,15 @@ $user_types_id = 1;
 $user_status = 'active';
 $password = hash("sha256", $_POST["passwrod"]);
 
+if (!isset($first_name) || !isset($last_name) || !isset($email) || !isset($phone) || !isset($dob) || !isset($password)) {
+    throw new Error("Missing info");
+}
+
 $query = $mysqli->prepare("INSERT INTO users(first_name, last_name, email, phone_number, dob, cities_id, user_types_id, user_status, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
 $query->bind_param("sssisiiss", $first_name, $last_name, $email, $phone, $dob, $cities_id, $user_types_id, $user_status, $password);
 $query->execute();
 
-$response = [];
-$response["success"] = true;
-header("Location: http://localhost/tomato/tomatoFrontend/views/profile.html");
+if (!$mysqli)
+    echo ('Error' . $mysqli->error);
 
-
-echo json_encode($response);
+header("Location: http://localhost/tomato/tomatoFrontend/index.html");
