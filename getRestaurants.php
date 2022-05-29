a3943140834abff6423b752923aad47116976efb
Khaled Faour <khaledfaour2@gmail.com>
2022-05-29 20:25:25 +0300

    Added getRestaurants API

---
+++ getRestaurants.php
@@ -0,0 +1,13 @@
+<?php
+include("connection.php");
+$query = $mysqli->prepare("SELECT * from restaurants");
+$query->execute();
+$array = $query->get_result();
+$response = [];
+while($todo = $array->fetch_assoc()){
+    $response[] = $todo;
+} 
+$json = json_encode($response);
+echo $json;
+
+?>
