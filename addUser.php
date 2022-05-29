8af388527f32213f7c4c3c3d7dee0326192df0f6
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-29 13:23:15 +0300

    Added first working api to add a user

---
+++ addUser.php
@@ -0,0 +1,22 @@
+<?php
+
+include("connection.php");
+
+$first_name = $_POST["first"];
+$last_name = $_POST["last"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$dob = $_POST["phone"];
+$cities_id = 1;
+$user_types_id = 1;
+$user_status = 'active';
+$password = hash("sha256", $_POST["passwrod"]);
+
+$query = $mysqli->prepare("INSERT INTO users(first_name, last_name, email, phone_number, dob, cities_id, user_types_id, user_status, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$query->bind_param("sssisiiss", $first_name, $last_name, $email, $phone, $dob, $cities_id, $user_types_id, $user_status, $password);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
