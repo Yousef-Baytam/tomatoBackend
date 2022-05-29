f2ec5bb4dfe6dc5b5db50838bae38122a1d2bbd4
Khaled Faour <khaledfaour2@gmail.com>
2022-05-29 20:56:08 +0300

    Fixed getRestaurants API

--- getRestaurants.php
+++ getRestaurants.php
@@ -1,6 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
 include("connection.php");
-$query = $mysqli->prepare("SELECT * from restaurants");
+$query = $mysqli->prepare("SELECT *, (SELECT AVG(rating) FROM reviews WHERE reviews.restaurants_id = r.id) as rate FROM restaurants r;");
 $query->execute();
 $array = $query->get_result();
 $response = [];
