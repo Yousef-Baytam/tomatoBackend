b7d7b0045c2d5a551dd60cdbc6e6d776f7422696
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-30 20:19:46 +0300

    Fixed the Restaurants api

--- getAllRes.php
+++ getAllRes.php
@@ -1,24 +1,14 @@
 <?php
 include("connection.php");
 
-$query = $mysqli->prepare("SELECT r.name, r.description, r.id, r.image, r.status, AVG(rev.rating), c.name, cat.category FROM restaurants r, reviews rev, cities c, categories_has_restaurants cath, categories cat WHERE rev.restaurants_id = r.id and r.cities_id = c.id and cath.restaurants_id = r.id and cath.categories_id = cat.id group by r.id");
+$query = $mysqli->prepare("SELECT r.id, r.name, r.description, r.image, r.status, AVG(rev.rating), c.name, cat.category, t.type FROM types t, types_has_restaurants thr, restaurants r, reviews rev, cities c, categories_has_restaurants cath, categories cat WHERE rev.restaurants_id = r.id and r.cities_id = c.id and cath.restaurants_id = r.id and cath.categories_id = cat.id and r.id = thr.restaurants_id and thr.types_id = t.id group by r.id");
 $query->execute();
-$query->store_result();
-$num_rows = $query->num_rows;
-$query->bind_result($n, $l, $e, $p, $pp, $s, $d, $t);
-$query->fetch();
+$array = $query->get_result();
 $response = [];
-if ($num_rows == 0) {
-    $response["response"] = "User Not Found";
-} else {
-    $response["name"] = $n;
-    $response["last"] = $l;
-    $response["email"] = $e;
-    $response["phone"] = $p;
-    $response["profilePic"] = $pp;
-    $response["status"] = $s;
-    $response["dob"] = $d;
-    $response["type"] = $t;
+
+while ($item = $array->fetch_assoc()) {
+    $response[] = $item;
 }
+
 $json = json_encode($response);
 echo $json;
