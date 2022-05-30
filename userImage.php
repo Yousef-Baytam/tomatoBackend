ebc88946c3fea964adaf2496cb652c310a7c87de
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-30 18:17:04 +0300

    Added an api to update user profile pic

--- userImage.php
+++ userImage.php
@@ -1,1 +1,21 @@
+<?php
+include("connection.php");
 
+if (!isset($_POST["img"]) || !isset($_POST["id"])) {
+    die('fuck you');
+};
+
+$img = base64_encode($_POST["img"]);
+$id = $_POST["id"];
+
+$query = $mysqli->prepare("Update users SET profile_pic = ? WHERE id= ?");
+$query->bind_param('si', $img, $id);
+$query->execute();
+
+$nrows = $mysqli->affected_rows;
+if (!$nrows) {
+    echo  $nrows . '   failed';
+}
+
+if (!$mysqli)
+    echo ('Error' . $mysqli->error);
