8b4520f0df349a787ec083a1e8aee5f0a0d3a3b6
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-31 01:22:09 +0300

    Added the city name to the usergetinfo api

--- getUser.php
+++ getUser.php
@@ -2,12 +2,12 @@
 include("connection.php");
 
 $id = $_GET["id"];
-$query = $mysqli->prepare("Select u.first_name, u.last_name, u.email, u.phone_number, u.profile_pic, u.user_status, u.dob, t.user_type FROM users u, user_types t WHERE u.id = ? and u.user_types_id = t.id");
+$query = $mysqli->prepare("Select u.first_name, u.last_name, u.email, u.phone_number, u.profile_pic, u.user_status, u.dob, t.user_type, c.city_name FROM users u, user_types t, cities c WHERE u.id = ? and u.user_types_id = t.id and u.cities_id = c.id");
 $query->bind_param("i", $id);
 $query->execute();
 $query->store_result();
 $num_rows = $query->num_rows;
-$query->bind_result($n, $l, $e, $p, $pp, $s, $d, $t);
+$query->bind_result($n, $l, $e, $p, $pp, $s, $d, $t, $c);
 $query->fetch();
 $response = [];
 $pp = base64_decode($pp);
@@ -22,6 +22,7 @@ if ($num_rows == 0) {
     $response["status"] = $s;
     $response["dob"] = $d;
     $response["type"] = $t;
+    $response["city"] = $c;
 }
 $json = json_encode($response);
 echo $json;
