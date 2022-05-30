fa8da5eeedd60d6c04fe31c4a33f3341f263b609
Khaled Faour <khaledfaour2@gmail.com>
2022-05-30 12:36:32 +0300

    Fixed getRestaurants AVG Calculation

--- getRestaurants.php
+++ getRestaurants.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json');
 include("connection.php");
-$query = $mysqli->prepare("SELECT *, (SELECT AVG(rating) FROM reviews WHERE reviews.restaurants_id = r.id) as rate FROM restaurants r;");
+$query = $mysqli->prepare("SELECT *, (SELECT AVG(rating) FROM reviews WHERE reviews.restaurants_id = r.id AND reviews.status = 'accepted') as rate FROM restaurants r;");
 $query->execute();
 $array = $query->get_result();
 $response = [];
